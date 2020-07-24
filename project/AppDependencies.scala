a2f90122e9fc1bc33fc0233567cc0507be3e2088
girishkamat <girish.kamat@gmail.com>
2020-07-24 12:22:00 +0100

    GFORMS-840 Configuration and scheduling of consolidator jobs, using akka-quartz-scheduler

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,9 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "2.23.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo"      % "7.30.0-play-27"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-27" % "2.23.0",
+    "uk.gov.hmrc"       %% "simple-reactivemongo"      % "7.30.0-play-27",
+    "com.enragedginger" %% "akka-quartz-scheduler"     % "1.8.1-akka-2.5.x"
   )
 
   val test = Seq(
@@ -19,6 +20,7 @@ object AppDependencies {
     "com.typesafe.play"      %% "play-test"                 % current   % Test,
     "org.mockito"            %% "mockito-scala"             % "1.14.8"  % Test,
     "org.mockito"            %% "mockito-scala-scalatest"   % "1.14.8"  % Test,
+    "com.typesafe.akka"      %% "akka-testkit"              % "2.5.31"  % Test,
     "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"        % "4.0.3"   % "test, it"
   )
